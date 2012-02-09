0ca35caf724e5c6a332f00f90f0953a99cdb02f3
Kevin Dew <kev@dewsolutions.co.uk>
2012-02-09 02:00:23 +0000

    Initial Commit

---
+++ View Bounds/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  View Bounds
+//
+//  Created by Kevin Dew on 09/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
